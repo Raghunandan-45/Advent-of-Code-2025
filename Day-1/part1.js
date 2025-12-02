73db9f6cdda0525094a45a93db8c5bda6a90720f
Raghunandan <araghunandan2005@gmail.com>
2025-12-02 12:51:46 +0530

    Final code for part1 of day 1

--- Day-1/part1.js
+++ Day-1/part1.js
@@ -11,68 +11,11 @@ fs.readFile("01.txt","utf-8", (err,data) => {
     console.log(arr);
     console.log(`Length - ${arr.length}`); // ES6 convention
     let il = arr.length;
-    /*
-    // Circular linked list implementation
-    function circularLinkedList(){
-        let Node = function(element){
-            this.element = element;
-            this.next = null;
-            this.prev = null;
-        }
-
-        let length = 0;
-        let head = null;
-
-        //Get element at specific index
-        this.getElementAt = function(index){
-            if(index>=0 && index<=length){
-                let node = head;
-                for(let i =0;i<index && node!=null;i++){
-                    node = node.next;
-                }
-                return node;
-            }
-            return undefined;
-        }
-
-        //Appending new node
-        this.append = function(element){
-            
-            //Create new Node
-            const node = new Node(element);
-            let current;
-
-            //If head is empty
-            //Then make new node head
-
-            if(head === null){
-                head = node;
-            }
-            else{
-                //Add the new node as the next node
-                //Then mark the next of new node to head
-                current = this.getElementAt(length-1);
-                current.next = node;
-            }
-
-            node.next = head;
-            length++;
-
-        }
-    }
-
-    //Creating a dial starting from 0 to 99
-    let cll = new circularLinkedList();
-    for (let i=0;i<=99;i++){
-        cll.append(i);
-        console.log("Element at ",i,"th index is: ",i);
-    }
-    */
+    
     let pos = 50;
     let count = 0;
 
     for(const move of arr){
-        //console.log(`${i}th element is ${arr[i]}`);
         const dir = move[0];
         const steps = parseInt(move.substring(1),10);
 
